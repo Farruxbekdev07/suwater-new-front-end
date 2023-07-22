1f28d8826c8785797508e4bc00bc5de179779127
Xondamir Mo'minov <76545982+xondamirmuminov@users.noreply.github.com>
2023-07-22 16:28:49 +0500

    feat: add tailwind and font family (#1)

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -9,6 +9,10 @@ export function HomePage() {
         <meta name="description" content="A Boilerplate application homepage" />
       </Helmet>
       <span>My HomePage</span>
+      <h1 className="text-3xl font-medium">Katta salom</h1>
+      <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+        Badge
+      </span>
     </>
   );
 }
